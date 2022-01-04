28049e74b8cad1eac8b9888c740b99b540f7a643
philip-loeffler <philiploeffler@gmail.com>
2022-01-04 11:28:05 -0500

    types

--- Section2/Variables.py
+++ Section2/Variables.py
@@ -2,3 +2,7 @@ day_hours = 24
 week_days = 7
 
 print(day_hours * week_days)
+
+week_hours = day_hours * week_days
+
+print(week_hours)
---
+++ Section3/simpleTypes.py
@@ -0,0 +1,13 @@
+# this is implicit decarlartions of variables, python doesnt require you to define the type
+x = 10
+y = "10"
+z = 10.1
+
+sum1 = x + x 
+sum2 = y + y
+
+print(sum1, sum2)
+
+# type function to find type
+# z if a float, i know you're going to forget that so here is a friendly reminder
+print(type(x), type(y), type(z))
