5b8dbb839c68cf6002e4ab9e054a601b91e5b562
philip-loeffler <philiploeffler@gmail.com>
2022-01-05 09:41:07 -0500

    lists

---
+++ Section3/listType.py
@@ -0,0 +1,20 @@
+
+student_grades = [9.1, 8.8, 7.5]
+
+
+# the mean value of student grades
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+
+
+
+
+# allows you to check what attributes the object have
+print(dir())
+print(dir(str))
+print(dir(list))
+
+
+print(dir(__builtins__))
