853b620d5d73bbde03c189a932ac0f0587d2802c
philip-loeffler <philiploeffler@gmail.com>
2022-01-07 13:47:13 -0500

    while

---
+++ Section7/whileLoops.py
@@ -0,0 +1,13 @@
+a = 3
+
+
+# this is infinite 
+while a > 0:
+    print(1)
+
+
+
+user_name = ''
+while username != "pypy":
+    user_name = input("enter username")
+
---
+++ Section7/whileWithBreaks.py
@@ -0,0 +1,8 @@
+
+# loops terminate when the user puts in pypy.
+while True:
+    user_name = input("enter username")
+    if username == 'pypy':
+        break
+    else:
+        continue
