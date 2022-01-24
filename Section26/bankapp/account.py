aa4222f9a718be57776c74563cc206085f6d0442
philip-loeffler <philiploeffler@gmail.com>
2022-01-24 10:22:36 -0500

    inheritance

--- Section26/bankapp/account.py
+++ Section26/bankapp/account.py
@@ -24,3 +24,26 @@ print(account.balance)
 account.withdraw(100)
 print(account.balance)
 account.commit()
+
+
+# demonstrating inheritance with another class
+# purpose it to inherit methods to parent class into child class
+# you need to pass the parent to the child
+class Checking(Account):
+    def __init__(self, filepath, fee):
+        # when checking is created, you are also creating an account object as well
+        Account.__init__(self, filepath)
+        # fee is your own instance variable to this class
+        self.fee = fee
+    
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+
+checking = checking("/balance.txt", 1)
+
+# purpose it to inherit methods to parent class into child class
+checking.deposit(10)
+checking.transfer(110)
+print(checking.balance)
+checking.commit()
